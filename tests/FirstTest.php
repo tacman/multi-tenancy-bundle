5f49832e05a7a210af569161a3759eff2d9e15b8
fouadsalkini <fouad@sync.com.lb>
2023-12-31 20:57:33 +0200

    added first test

---
+++ tests/FirstTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(true); // Replace with your actual test logic
+    }
+}
